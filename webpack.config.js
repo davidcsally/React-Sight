79bec86e3fa3706c156d20178bb914c060d15d0f
David Sally <davidcsally@gmail.com>
2018-01-21 18:00:57 -0800

    [Update] add notes for new features

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,22 @@
+/* eslint no-unused-vars: 0 */
+const path = require('path');
 const webpack = require('webpack');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
     bundle: './chrome-ext/frontend/devtools.js',
     installHook: './chrome-ext/backend/installHook.js',
+    style: './chrome-ext/css/style.css',
   },
   output: {
     filename: '[name].js',
     path: `${__dirname}/chrome-ext/build/`,
   },
-  // use a load for .jsx and ES6
+  // devServer: {
+  //   contentBase: `${__dirname}/chrome-ext/build`,
+  // },
+  // use a load for .js and ES6
   module: {
     loaders: [
       {
@@ -21,9 +28,19 @@ module.exports = {
         },
       },
       {
-        test: /\.css$/,
-        loaders: ['style-loader', 'css-loader'],
+        test: /\.sass$|\.scss$|\.css$/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader'],
       },
     ],
   },
+  // plugins: [
+  // TODO - prod and test env vars
+  // new webpack.DefinePlugin({
+  //   'process.env.NODE_ENV': JSON.stringify('test'),
+  // }),
+  // TODO - generate HTML during build
+  //   new HtmlWebpackPlugin({
+  //     template: `${__dirname}/chrome-ext/devtools.html`,
+  //   }),
+  // ],
 };
