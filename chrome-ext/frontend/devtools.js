ccac8798527c16d1795bb1daa9cbc05af7c28172
David Sally <davidcsally@gmail.com>
2018-01-21 18:14:28 -0800

    [Add] basic support for webpack dev server

--- chrome-ext/frontend/devtools.js
+++ chrome-ext/frontend/devtools.js
@@ -6,6 +6,8 @@ import drawLoadingScreen from './loader';
 import * as drawChart from './drawChart';
 import drawBreadcrumbs from './breadcrumb';
 import { filterRedux, filterRouter, filterDOM } from './filters';
+import { mockDOM } from '../../test/fixtures';
+import '../css/style.css';
 
 // stores last snapshot of data
 let curData;
@@ -45,41 +47,57 @@ const draw = () => {
   drawBreadcrumbs(processedData.data[0]);
 };
 
-// ****************`
+// ****************
 // ***** MAIN *****
 // ****************
-// attach panel to chrome dev tools
-chrome.devtools.panels.create('React-Sight', null, 'devtools.html', () => {
-  // wire up buttons to actions
-  document.querySelector('#router-btn').addEventListener('click', draw);
-  document.querySelector('#redux-btn').addEventListener('click', draw);
-  document.querySelector('#dom-btn').addEventListener('click', draw);
-  document.querySelector('#zoom-in-btn').addEventListener('click', drawChart.zoomIn);
-  document.querySelector('#zoom-out-btn').addEventListener('click', drawChart.zoomOut);
-
-  // call a zoom in / zoom out to fix first pan/drag event,
-  // without this, first dragging chart will cause it to jump on screen
-  // drawChart.zoomIn();
-  // drawChart.zoomOut();
-
-  const port = chrome.extension.connect({
-    name: 'React-Sight',
-  });
-
-  // establishes a connection between devtools and background page
-  port.postMessage({
-    name: 'connect',
-    tabId: chrome.devtools.inspectedWindow.tabId,
-  });
-
-  // appends loading screen
-  drawLoadingScreen();
-
-  // Listens for posts sent in specific ports and redraws tree
-  port.onMessage.addListener((msg) => {
-    if (!msg.data) return; // abort if data not present, or if not of type object
-    if (typeof msg !== 'object') return;
-    curData = msg; // assign global data object
+if (process.env.NODE_ENV === 'development') {
+  // TODO - set up webpack to pass NODE_ENV + build .html file from webpack
+  console.log('node env: ', process.env.NODE_ENV);
+  curData = mockDOM;
+  document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('#router-btn').addEventListener('click', draw);
+    document.querySelector('#redux-btn').addEventListener('click', draw);
+    document.querySelector('#dom-btn').addEventListener('click', draw);
+    document.querySelector('#zoom-in-btn').addEventListener('click', drawChart.zoomIn);
+    document.querySelector('#zoom-out-btn').addEventListener('click', drawChart.zoomOut);
+    drawLoadingScreen();
     draw();
   });
-});
+} else {
+  // attach panel to chrome dev tools
+  chrome.devtools.panels.create('React-Sight', null, 'devtools.html', () => {
+    // wire up buttons to actions
+    document.querySelector('#router-btn').addEventListener('click', draw);
+    document.querySelector('#redux-btn').addEventListener('click', draw);
+    document.querySelector('#dom-btn').addEventListener('click', draw);
+    document.querySelector('#zoom-in-btn').addEventListener('click', drawChart.zoomIn);
+    document.querySelector('#zoom-out-btn').addEventListener('click', drawChart.zoomOut);
+
+    // call a zoom in / zoom out to fix first pan/drag event,
+    // without this, first dragging chart will cause it to jump on screen
+    // drawChart.zoomIn();
+    // drawChart.zoomOut();
+
+    const port = chrome.extension.connect({
+      name: 'React-Sight',
+    });
+
+    // establishes a connection between devtools and background page
+    port.postMessage({
+      name: 'connect',
+      tabId: chrome.devtools.inspectedWindow.tabId,
+    });
+
+    // appends loading screen
+    drawLoadingScreen();
+
+    // Listens for posts sent in specific ports and redraws tree
+    port.onMessage.addListener((msg) => {
+      if (!msg.data) return; // abort if data not present, or if not of type object
+      if (typeof msg !== 'object') return;
+      // TODO - dont call draw if msg === curData
+      curData = msg; // assign global data object
+      draw();
+    });
+  });
+}
